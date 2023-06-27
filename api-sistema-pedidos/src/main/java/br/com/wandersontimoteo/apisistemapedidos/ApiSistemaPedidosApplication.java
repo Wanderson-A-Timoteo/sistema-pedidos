15935bd080f3db5811dc27ba7fb956cae576fc85
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-06-27 19:34:04 -0400

    Project created

---
+++ api-sistema-pedidos/src/main/java/br/com/wandersontimoteo/apisistemapedidos/ApiSistemaPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.wandersontimoteo.apisistemapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaPedidosApplication.class, args);
+	}
+
+}
---
+++ api-sistema-pedidos/src/test/java/br/com/wandersontimoteo/apisistemapedidos/ApiSistemaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wandersontimoteo.apisistemapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
