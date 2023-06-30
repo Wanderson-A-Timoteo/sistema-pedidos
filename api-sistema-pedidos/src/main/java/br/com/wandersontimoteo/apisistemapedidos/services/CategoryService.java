6763889256579066588a88cb89d8a51a8ff1a3e7
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-06-30 10:16:12 -0400

    Category

---
+++ api-sistema-pedidos/src/main/java/br/com/wandersontimoteo/apisistemapedidos/services/CategoryService.java
@@ -0,0 +1,25 @@
+package br.com.wandersontimoteo.apisistemapedidos.services;
+
+import br.com.wandersontimoteo.apisistemapedidos.entities.Category;
+import br.com.wandersontimoteo.apisistemapedidos.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+    }
+}
