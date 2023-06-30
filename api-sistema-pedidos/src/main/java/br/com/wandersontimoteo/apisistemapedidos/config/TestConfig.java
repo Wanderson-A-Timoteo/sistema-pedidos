cf2b283e257ca5bcb65861971ded9cc1a26850ad
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-06-30 14:46:31 -0400

    OrderItem, many-to-many association with extra attributes PART 2

--- api-sistema-pedidos/src/main/java/br/com/wandersontimoteo/apisistemapedidos/config/TestConfig.java
+++ api-sistema-pedidos/src/main/java/br/com/wandersontimoteo/apisistemapedidos/config/TestConfig.java
@@ -1,14 +1,8 @@
 package br.com.wandersontimoteo.apisistemapedidos.config;
 
-import br.com.wandersontimoteo.apisistemapedidos.entities.Category;
-import br.com.wandersontimoteo.apisistemapedidos.entities.Order;
-import br.com.wandersontimoteo.apisistemapedidos.entities.Product;
-import br.com.wandersontimoteo.apisistemapedidos.entities.User;
+import br.com.wandersontimoteo.apisistemapedidos.entities.*;
 import br.com.wandersontimoteo.apisistemapedidos.entities.enums.OrderStatus;
-import br.com.wandersontimoteo.apisistemapedidos.repositories.CategoryRepository;
-import br.com.wandersontimoteo.apisistemapedidos.repositories.OrderRepository;
-import br.com.wandersontimoteo.apisistemapedidos.repositories.ProductRepository;
-import br.com.wandersontimoteo.apisistemapedidos.repositories.UserRepository;
+import br.com.wandersontimoteo.apisistemapedidos.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -46,13 +43,6 @@ public class TestConfig implements CommandLineRunner {
         Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
-
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
@@ -65,7 +55,22 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
     }
 }
--- api-sistema-pedidos/src/main/java/br/com/wandersontimoteo/apisistemapedidos/entities/OrderItem.java
+++ api-sistema-pedidos/src/main/java/br/com/wandersontimoteo/apisistemapedidos/entities/OrderItem.java
@@ -1,6 +1,7 @@
 package br.com.wandersontimoteo.apisistemapedidos.entities;
 
 import br.com.wandersontimoteo.apisistemapedidos.entities.pkcomposite.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -15,7 +16,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
     private Double price;
@@ -31,6 +32,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
