fcc97c34a986e1e168ea6fb51b16a74fed6ee08a
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-10-14 19:52:47 -0400

    Delete customResponse

--- api-sistema-pedidos/src/main/java/br/com/wandersontimoteo/apisistemapedidos/resources/OrderResource.java
+++ api-sistema-pedidos/src/main/java/br/com/wandersontimoteo/apisistemapedidos/resources/OrderResource.java
@@ -4,10 +4,7 @@ import br.com.wandersontimoteo.apisistemapedidos.entities.Order;
 import br.com.wandersontimoteo.apisistemapedidos.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
